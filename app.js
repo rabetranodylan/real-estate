93ef3dedf3b765f7402686313ed3df7e79ba3153
dogmos <rabetranodylan@yahoo.fr>
2017-02-03 17:16:42 +0100

    Start TP3

--- app.js
+++ app.js
@@ -12,14 +12,62 @@ app.set( 'view engine', 'ejs' );
 //setting the 'assets' directory as our static assets dir (css, js, img, etc...)
 app.use( '/assets', express.static( 'assets' ) );
 
+function callLeboncoin() {
+
+    var url = 'https://www.leboncoin.fr/ventes_immobilieres/1084457186.htm?ca=12_s'
+
+    request( url, function ( error, response, html ) {
+
+        if ( !error && response.statusCode == 200 ) {
+
+            var $ = cheerio.load( html )
+
+            var lbcDataArray = $( 'section.properties span.value' )
+
+            var lbcData = {
+
+                price: parseInt( $( lbcDataArray.get( 0 ) ).text().replace( /\s/g, '' ), 10 ),
+
+                city: $( lbcDataArray.get( 1 ) ).text().trim().toLowerCase().replace( /\_|\s/g, '-' ),
+
+                type: $( lbcDataArray.get( 2 ) ).text().trim().toLowerCase(),
+
+                surface: parseInt( $( lbcDataArray.get( 4 ) ).text().replace( /\s/g, '' ), 10 )
+
+            }
+
+            console.log( lbcData )
+
+        }
+
+        else {
+
+            console.log( error )
+
+        }
+
+    })
+
+}
 
 //makes the server respond to the '/' route and serving the 'home.ejs' template in the 'views' directory
 app.get( '/', function ( req, res ) {
+
+    callLeboncoin();
+
     res.render( 'home', {
         message: 'The Home Page!'
     });
 });
 
+app.get( '/call', function ( req, res ) {
+
+    var url = req.query.urlLBC
+
+    res.render( 'home', {
+        message: url
+    });
+});
 
 //launch the server on the 3000 port
 app.listen( 3000, function () {
