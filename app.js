76f520c037cf4ad5c546c0064dd589e8e2f7254f
dogmos <rabetranodylan@yahoo.fr>
2017-02-21 21:54:02 +0100

    TP3

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ function callLeboncoin() {
 
     var url = 'https://www.leboncoin.fr/ventes_immobilieres/1084457186.htm?ca=12_s'
 
+
     request( url, function ( error, response, html ) {
 
         if ( !error && response.statusCode == 200 ) {
@@ -34,9 +35,16 @@ function callLeboncoin() {
 
                 surface: parseInt( $( lbcDataArray.get( 4 ) ).text().replace( /\s/g, '' ), 10 )
 
+
+
             }
 
+            var pricem2 = lbcData.price / lbcData.surface;
+
             console.log( lbcData )
+            console.log( "price m2 = " + pricem2 )
+
+
 
         }
 
@@ -50,10 +58,38 @@ function callLeboncoin() {
 
 }
 
+function callMeilleursAgents() {
+
+    var urlMa = 'https://www.meilleursagents.com/prix-immobilier/poissy-78300/'
+
+    request( urlMa, function ( error, response, html ) {
+        if ( !error && response.statusCode == 200 ) {
+
+            const $ = cheerio.load( html )
+
+            const maDataArray = $( 'div.prices-summary div.prices-summary__cell--median' );
+
+            let maData = {
+                prixappart: parseInt( $( maDataArray.get( 0 ) ).text().replace( /\s/g, '' ), 10 ),
+                prixmaison: parseInt( $( maDataArray.get( 1 ) ).text().replace( /\s/g, '' ), 10 ),
+                loyer: parseFloat( $( maDataArray.get( 2 ) ).text().replace( /\s/g, '' ), 10 )
+
+            }
+            console.log( maData )
+
+        }
+
+        else {
+            console.log( error )
+        }
+    })
+}
+
 //makes the server respond to the '/' route and serving the 'home.ejs' template in the 'views' directory
 app.get( '/', function ( req, res ) {
 
     callLeboncoin();
+    callMeilleursAgents();
 
     res.render( 'home', {
         message: 'The Home Page!'
